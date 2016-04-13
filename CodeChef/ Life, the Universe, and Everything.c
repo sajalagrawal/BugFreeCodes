b181362c5089ee45906f7626f89aa4d093f9901e
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-13 23:00:58 +0530

    Create  Life, the Universe, and Everything.c

---
+++ CodeChef/ Life, the Universe, and Everything.c
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/TEST
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include<stdio.h>
+main(){
+int n;
+while(1){
+scanf("%d",&n);
+if(n==42)
+break;
+else
+printf("%d\n",n);
+}
+return 0;
+}  
