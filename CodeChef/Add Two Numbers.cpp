f54594fbe3092d916c7bb3b2ab4bb4a73020164f
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-13 23:50:41 +0530

    Create Add Two Numbers.cpp

---
+++ CodeChef/Add Two Numbers.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/FLOW001
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+int t,i,a,b;
+cin>>t;
+for(i=0;i<t;i++)
+{
+cin>>a>>b;
+cout<<a+b<<"\n";
+}
+return 0;
+}
