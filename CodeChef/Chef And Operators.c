ffdc0dc0f53e758ef3856f22fde23c5fcbd64eb4
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-13 23:41:13 +0530

    Create Chef And Operators.c

---
+++ CodeChef/Chef And Operators.c
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/CHOPRT
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <stdio.h>
+ 
+int main()
+{
+    int t,a,b,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%d %d",&a,&b);
+        if(a>b)
+            printf(">\n");
+        else if(a<b)
+            printf("<\n");
+        else
+            printf("=\n");
+    }
+    return 0;
+}
