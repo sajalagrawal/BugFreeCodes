02464b21c0f2161c508f1a1f6bfe121cdad329e3
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-15 00:51:39 +0530

    Create  Servant.cpp

---
+++ CodeChef/ Servant.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/FLOW008
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include<iostream>
+using namespace std;
+int main(){
+int t,n;
+cin>>t;
+while(t--){
+cin>>n;
+if(n<10)
+cout<<"What an obedient servant you are!\n";
+else
+cout<<"-1\n";
+}
+return 0;
+} 
