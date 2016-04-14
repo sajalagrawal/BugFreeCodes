75e65188a89f754348a97b045cc4abe850a7d0b1
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-15 00:33:43 +0530

    Create Number Mirror.cpp

---
+++ CodeChef/Number Mirror.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/START01
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include<iostream>
+using namespace std;
+ 
+int main(){
+int a;
+cin>>a;
+cout<<a;
+return 0;
+} 
