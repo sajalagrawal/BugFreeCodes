6dce3a5989b579cfa7b7a323c4248dcf877216a7
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-15 00:31:34 +0530

    Create Find Remainder.cpp

---
+++ CodeChef/Find Remainder.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/FLOW002
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+int t,a,b;
+cin>>t;
+while(t--){
+cin>>a>>b;
+cout<<a%b<<"\n";
+}
+return 0;
+} 
