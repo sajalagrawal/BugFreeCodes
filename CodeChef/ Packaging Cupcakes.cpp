3c7d45245e31967f17c04c086b3838c3dffeee73
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-04-18 11:50:06 +0530

    Create  Packaging Cupcakes.cpp

---
+++ CodeChef/ Packaging Cupcakes.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/MUFFINS3
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n,t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n/2+1<<"\n";
+    }
+    return 0;
+}
