ad5130a876b1c922d122e4721e3ca2291a6996f7
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-05-15 11:35:23 +0530

    Create Manasa and Stones.cpp

---
+++ HackerRank/Manasa and Stones.cpp
@@ -0,0 +1,34 @@
+//https://www.hackerrank.com/challenges/manasa-and-stones
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int t,n,a,b,j;
+    cin>>t;
+    while(t--){            
+        cin>>n>>a>>b;
+        int res=0;
+        for(j=0;j<=n-1;j++){
+            if(a==b){
+                res=a*(n-1);
+                cout<<res<<" ";
+                break;
+            }
+            else if(a>b){
+                res=a*j+b*(n-1-j);
+                cout<<res<<" ";
+            }
+            else{
+                res=b*j+a*(n-1-j);
+                cout<<res<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
