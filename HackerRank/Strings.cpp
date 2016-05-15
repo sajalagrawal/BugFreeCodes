bcf5dbbe94fabe14481e01f7b2d619a04ce047d0
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-05-15 11:52:34 +0530

    Create Strings.cpp

---
+++ HackerRank/Strings.cpp
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/c-tutorial-strings
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   // Complete the program
+    string a,b;
+    cin>>a;
+    cin>>b;
+    cout<<a.size()<<" "<<b.size();
+    cout<<"\n"<<(a+b)<<"\n";
+    char m,n,t;
+    t=a[0];
+    a[0]=b[0];
+    b[0]=t;
+    cout<<a<<" "<<b;
+  
+    return 0;
+}
