16293380384ba4d0c4a5099fc923f20cd6b1adab
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-07-23 00:01:26 +0530

    Create  Turbo Sort.cpp

---
+++ CodeChef/ Turbo Sort.cpp
@@ -0,0 +1,24 @@
+//https://www.codechef.com/problems/TSORT/
+//Author - Sajal Agrawal
+//@sajalagrawal
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a[1000001]={0,},n,t,i;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        a[n]++;
+    }
+    for(i=0;i<1000001;i++){
+        while(a[i]>0){
+            cout<<i<<"\n";
+            a[i]--;  //for repeated occurrences
+        }
+    }
+    return 0;
+}
