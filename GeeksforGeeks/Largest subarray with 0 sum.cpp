413089ccb50ea7212a1728eaafaa36c74e78b708
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-10-15 21:31:56 +0530

    Create Largest subarray with 0 sum.cpp

---
+++ GeeksforGeeks/Largest subarray with 0 sum.cpp
@@ -0,0 +1,18 @@
+//http://www.practice.geeksforgeeks.org/problem-page.php?pid=700254
+//Author- Sajal Agrawal
+//Username:sajalagrawal
+
+/*You are required to complete this method*/
+int max_sum(int A[],int n)
+{
+    int sum=0,i,max=0,j,k;
+    for(i=0;i<n;i++){
+        sum=0;
+        for(j=0;j<n;j++){
+            k=(i+j)%n;
+            sum+=A[j]*k;
+        }
+        if(sum>max)max=sum;
+    }
+    return max;
+}
