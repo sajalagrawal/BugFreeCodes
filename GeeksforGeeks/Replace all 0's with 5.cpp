99c924b6712ff2614f684cb9fb4ee81bf628da4c
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-10-15 21:10:44 +0530

    Create Replace all 0's with 5.cpp

---
+++ GeeksforGeeks/Replace all 0's with 5.cpp
@@ -0,0 +1,23 @@
+//http://www.practice.geeksforgeeks.org/problem-page.php?pid=700276
+//Author- Sajal Agrawal
+//Username:sajalagrawal
+
+/*you are required to complete this method*/
+int convertFive(int n)
+{
+    int rev=0,k;
+    while(n!=0){
+        k=n%10;
+        if(k==0)k=5;
+        rev=(rev*10)+k;
+        n=n/10;
+    }
+    n=rev;
+    rev=0;
+    while(n!=0){
+        k=n%10;
+        rev=(rev*10)+k;
+        n/=10;
+    }
+    return rev;
+}
