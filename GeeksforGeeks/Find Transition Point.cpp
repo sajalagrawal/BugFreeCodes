0b5272b578333b8fd6644f7befb8318904bac9c8
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-10-17 10:48:33 +0530

    Create Find Transition Point.cpp

---
+++ GeeksforGeeks/Find Transition Point.cpp
@@ -0,0 +1,10 @@
+//http://www.practice.geeksforgeeks.org/problem-page.php?pid=700194
+//Author- Sajal Agrawal
+//Username:sajalagrawal
+int transitionPoint(int arr[],int n)
+{
+    int i;
+    for(i=0;i<n;i++){
+        if(arr[i]==1)return i;
+    }
+}
