8fc412c6e80c55ac5d2ed64d7b382107e7fa612c
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-11-21 19:54:11 +0530

    Create Remainder Evaluation.cpp

---
+++ GeeksforGeeks/Remainder Evaluation.cpp
@@ -0,0 +1,16 @@
+//http://www.practice.geeksforgeeks.org/problem-page.php?pid=943
+//Author- Sajal Agrawal
+//@sajal.agrawal1997@gmail.com
+
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t,n,m;
+	cin>>t;
+	while(t--){
+	    cin>>n>>m;
+	    cout<<(n%m)<<"\n";
+	}
+	return 0;
+}
