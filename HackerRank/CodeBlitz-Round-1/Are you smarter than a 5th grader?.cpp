c14c2f95da416bbb8308128cd8b14544ca1d90d1
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2016-12-10 21:42:13 +0530

    Create Are you smarter than a 5th grader?.cpp

---
+++ HackerRank/CodeBlitz-Round-1/Are you smarter than a 5th grader?.cpp
@@ -0,0 +1,38 @@
+//https://www.hackerrank.com/contests/code-blitz-001/challenges/kya-ap-panchvi-pass-se-tez-hai
+//@sajal.agrawal1997@gmail.com
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<pow(floor(sqrt(n)),2)<<"\n";
+    }
+    return 0;
+}
