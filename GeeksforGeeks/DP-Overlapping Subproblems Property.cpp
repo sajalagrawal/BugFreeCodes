369ec8aabe2ccf11e51cf69f86629e22c25942d8
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-04-04 21:26:49 +0530

    Create DP-Overlapping Subproblems Property.cpp

---
+++ GeeksforGeeks/DP-Overlapping Subproblems Property.cpp
@@ -0,0 +1,31 @@
+//http://www.geeksforgeeks.org/dynamic-programming-set-1/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAX 50
+#define NIL -1
+ll lookup[MAX];
+
+void init(){
+    int i;
+    for(i=0;i<MAX;i++)lookup[i]=NIL;
+}
+
+ll fib(int n){
+    if(lookup[n]==NIL){
+        if(n<=1)
+            lookup[n]=n;
+        else
+            lookup[n]=fib(n-1)+fib(n-2);
+    }
+    return lookup[n];
+}
+int main() {
+	// your code goes here
+	int i=0;
+	init();
+	for(;i<MAX;i++)
+	    cout<<fib(i)<<" ";
+	return 0;
+}
