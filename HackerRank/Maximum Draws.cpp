2f43363e7bfe6f303becca637bd0aab30bfe5cee
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-05-03 00:57:11 +0530

    Create Maximum Draws.cpp

---
+++ HackerRank/Maximum Draws.cpp
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/maximum-draws
+//Author - Sajal Agrawal
+//sajal.agrawal1997@gmail.com
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<(n+1)<<endl;
+    }
+    return 0;
+}
