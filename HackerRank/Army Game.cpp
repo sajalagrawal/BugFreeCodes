567ead3b9148ed3548cc3e49559f3b2bbc340d8c
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-05-07 14:00:35 +0530

    Add files via upload

---
+++ HackerRank/Army Game.cpp
@@ -0,0 +1,15 @@
+//https://www.hackerrank.com/challenges/game-with-cells
+//Author - Sajal Agrawal
+//sajal.agrawal1997@gmail.com
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+    cin >> n >> m;
+    cout<<((n+1)/2)*((m+1)/2)<<endl;
+    return 0;
+}
