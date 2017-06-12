5ceadfe7194872c5cc439e3378db5a01d92c87ef
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-06-12 11:22:00 +0530

    Add files via upload

--- CodeChef/Hotel Palama Palace.cpp
+++ CodeChef/Hotel Palama Palace.cpp
@@ -4,31 +4,26 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-pair<ll,ll>p[5005];
+#define ll long long 
+#define MOD 1000000007
+pair<ll,ll> a[5001];
 
 int main() {
-	// your code goes here
-	ll t,n,q,r;
-	int i;
+    int t,n,q,i,j;
+    ll k;
     cin>>t;
     while(t--){
         cin>>n>>q;
-        for(i=0;i<n;i++){
-            cin>>p[i].first;
-            cin>>p[i].second;
-        }
-        while(q--){
-            cin>>r;
-            int flag=0;
-            for(i=0;i<n;i++){
-                if(p[i].first<=r and p[i].second>=r){
-                    flag=1;
+        for(i=0;i<n;i++)cin>>a[i].first>>a[i].second;
+        for(i=0;i<q;i++){
+            cin>>k;
+            for(j=0;j<n;j++){
+                if(a[j].first<=k and a[j].second>=k){
                     break;
                 }
             }
-            if(flag==1)cout<<"NO\n";
-            else cout<<"YES\n";
+            if(j==n)cout<<"YES\n";
+            else cout<<"NO\n";
         }
     }
 	return 0;
