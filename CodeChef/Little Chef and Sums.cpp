ecbc383af3c9591cae7a55781481cf2294a97a04
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2017-10-21 01:57:50 +0530

    Update Little Chef and Sums.cpp

--- CodeChef/Little Chef and Sums.cpp
+++ CodeChef/Little Chef and Sums.cpp
@@ -1,3 +1,33 @@
+//https://www.codechef.com/problems/CHEFSUM
+//Author- Sajal Agrawal
+//sajal.agrawal1997@gmail.com
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define MOD 1000000007
+
+int main() {
+    int t,n,i,index,minn,k;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        index=0;minn=INT_MAX;
+        for(i=0;i<n;i++){
+            cin>>k;
+            //cout<<"k="<<k<<endl;
+            if(k<minn){
+                //cout<<"k-"<<k<<endl;
+                minn=k;
+                index=i;
+            }
+        }
+        cout<<index+1<<"\n";
+    }
+    
+	return 0;
+}
+
 //https://www.codechef.com/SEPT17/problems/CHEFSUM
 //Author- Sajal Agrawal
 //sajal.agrawal1997@gmail.com
