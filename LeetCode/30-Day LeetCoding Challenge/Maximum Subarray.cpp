ab21260da7245438932bb796537c0c0b12013a15
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-03 22:07:44 +0530

    Create Maximum Subarray.cpp

---
+++ LeetCode/30-Day LeetCoding Challenge/Maximum Subarray.cpp
@@ -0,0 +1,16 @@
+//Kadane’s Algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        for(int i=0;i<nums.size();i++){
+            currSum+=nums[i];
+            if(currSum>maxSum){
+                maxSum=currSum;
+            }
+            if(currSum<0)currSum=0;
+        }
+        return maxSum;
+    }
+};
