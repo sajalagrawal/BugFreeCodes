fc78d4948b6ae32dd3198d9b1773ff13a497c2ec
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-07 21:31:24 +0530

    Create Group Anagrams.cpp

---
+++ LeetCode/30-Day LeetCoding Challenge/Group Anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string> >  mp;
+        vector<vector<string>> res;
+        for(string str : strs){
+            string copy = str;
+            sort(str.begin(),str.end());
+            mp[str].push_back(copy);   
+        }
+        
+        for(auto entry : mp){
+            vector<string> v;
+            for(auto e : entry.second){
+                //cout<<e<<endl;
+                v.push_back(e);
+            }
+            res.push_back(v);
+        }
+        return res; 
+    }
+};
