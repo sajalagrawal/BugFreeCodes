931b57452d3c9e7fc106262ab49fa7daaef27dd5
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-08 20:56:18 +0530

    Update Middle of the Linked List.cpp

--- LeetCode/30-Day LeetCoding Challenge/Middle of the Linked List.cpp
+++ LeetCode/30-Day LeetCoding Challenge/Middle of the Linked List.cpp
@@ -1,4 +1,29 @@
-//Sol1 - Calculating length first
+//Sol1 - Using fast & slow pointers
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        if(fast->next)return slow->next;
+        return slow;
+    }
+};
+
+
+//Sol2 - Calculating length first
 /**
  * Definition for singly-linked list.
  * struct ListNode {
