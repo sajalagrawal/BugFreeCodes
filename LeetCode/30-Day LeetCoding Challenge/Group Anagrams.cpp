f8719ae40a03353a99a96d741c0e58eeedb66f79
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-08 11:54:36 +0530

    Update Group Anagrams.cpp

--- LeetCode/30-Day LeetCoding Challenge/Group Anagrams.cpp
+++ LeetCode/30-Day LeetCoding Challenge/Group Anagrams.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -10,13 +12,33 @@ public:
         }
         
         for(auto entry : mp){
-            vector<string> v;
-            for(auto e : entry.second){
-                //cout<<e<<endl;
-                v.push_back(e);
-            }
-            res.push_back(v);
+            res.push_back(entry.second);
         }
         return res; 
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         map<string, vector<string> >  mp;
+//         vector<vector<string>> res;
+//         for(string str : strs){
+//             string copy = str;
+//             sort(str.begin(),str.end());
+//             mp[str].push_back(copy);   
+//         }
+        
+//         for(auto entry : mp){
+//             vector<string> v;
+//             for(auto e : entry.second){
+//                 //cout<<e<<endl;
+//                 v.push_back(e);
+//             }
+//             res.push_back(v);
+//         }
+//         return res; 
+//     }
+// };
