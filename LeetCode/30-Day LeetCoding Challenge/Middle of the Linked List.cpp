fb1ecd7a47c1b67ba05e9a73f4a5acd10a0a4731
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-08 20:43:50 +0530

    Create Middle of the Linked List.cpp

---
+++ LeetCode/30-Day LeetCoding Challenge/Middle of the Linked List.cpp
@@ -0,0 +1,27 @@
+//Sol1 - Calculating length first
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len=0;
+        ListNode *temp = head;
+        while(temp!=NULL){
+            len++;
+            temp=temp->next;
+        }
+        int mid=len/2;
+        while(mid--){
+            head=head->next;
+        }
+        return head;
+        //3      0 1 2                  3/2=1
+        //4      0 1 2 3                4/2=2
+    }
+};
