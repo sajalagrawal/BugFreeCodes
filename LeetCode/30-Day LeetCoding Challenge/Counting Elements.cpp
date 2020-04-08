679f1e2b6a4dae0b084641ee7de5f050b511c0fb
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-08 12:02:07 +0530

    Create Counting Elements.cpp

---
+++ LeetCode/30-Day LeetCoding Challenge/Counting Elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+        map<int, int> mp;
+        for(int i=0;i<arr.size();i++)mp[arr[i]]++;
+        int res=0;
+        for(int i=0;i<arr.size();i++){
+            if(mp[arr[i]+1])res++;
+        }
+        return res;
+    }
+};
+
+/*
+I/P = [0,1,2,3,3,5]
+O/P = 3
+0 -> 1
+1 -> 2
+2 -> 3 2 is counted only once
+
+I/P = [1,1,2,3]
+O/P = 3
+1 -> 2
+1 -> 2
+2 -> 3
+*/
