fcd5ccb90316c235096c9d2e3e57a3b99545b274
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2020-04-20 10:45:07 +0530

    Create Minimum Value to Get Positive Step by Step Sum.cpp

---
+++ LeetCode/Minimum Value to Get Positive Step by Step Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int mod(int x){
+        if(x<0)return -x;
+        return x;
+    }
+    int minStartValue(vector<int>& nums) {
+        int sum=0,res=1,pos=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]<0){
+        //         pos=1;
+        //         break;
+        //     }
+        // }
+        // if(pos==0)return 1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum<0)res=max(res, mod(sum)+1);
+        }
+        return res;
+    }
+};
