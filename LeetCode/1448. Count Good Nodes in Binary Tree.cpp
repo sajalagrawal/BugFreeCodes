3d97f457e14aaa063c7a2cd7379802333fc36638
Sajal Agrawal <16614262+sajalagrawal@users.noreply.github.com>
2021-11-10 23:33:19 +0530

    Create 1448. Count Good Nodes in Binary Tree.cpp

---
+++ LeetCode/1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,40 @@
+//MSFT
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        if(root == NULL) return 0;
+        return preOrderSolve(root, root->val);
+    }
+    
+    int preOrderSolve(TreeNode* node, int maxTillRoot) {
+        if(node==NULL)return 0;
+        
+        int countThisNode = 0;
+        if(node->val >= maxTillRoot) countThisNode = 1;
+        cout<<node->val<<" "<<maxTillRoot<<" "<<countThisNode<<endl;
+        int currMax = max(node->val, maxTillRoot);
+        if(node->left && node->right){
+            return countThisNode + 
+                preOrderSolve(node->left, currMax) +
+                preOrderSolve(node->right, currMax);
+        } else if(node->left) {
+            return countThisNode + preOrderSolve(node->left, currMax);
+        } else if(node->right) {
+            return countThisNode + preOrderSolve(node->right, currMax);
+        }
+        return countThisNode;
+    }
+};
