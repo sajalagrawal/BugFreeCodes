6ea5802cd8b9c3d54683e06372189fbfe66d6b45
Sajal Agrawal <16614262+sajalagrawal@users.noreply.github.com>
2025-03-18 15:20:34 +0530

    Create binary-tree-using-struct.cpp

---
+++ LeetCode/binary-tree-using-struct.cpp
@@ -0,0 +1,48 @@
+// C++ program for preorder traversals
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Structure of a Binary Tree Node
+struct Node {
+    int data;
+    struct Node *left, *right;
+    Node(int v)
+    {
+        data = v;
+        left = right = nullptr;
+    }
+};
+
+// Function to print preorder traversal
+void printPreorder(struct Node* node)
+{
+    if (node == nullptr)
+        return;
+
+    // Deal with the node
+    cout << node->data << " ";
+
+    // Recur on left subtree
+    printPreorder(node->left);
+
+    // Recur on right subtree
+    printPreorder(node->right);
+}
+
+// Driver code
+int main()
+{
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    // Function call
+    cout << "Preorder traversal of binary tree is: \n";
+    printPreorder(root);
+
+    return 0;
+}
