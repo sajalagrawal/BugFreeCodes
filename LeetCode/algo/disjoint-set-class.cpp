3077e2b267f48f6010cbf830a25b3acafb53f847
Sajal Agrawal <16614262+sajalagrawal@users.noreply.github.com>
2025-03-31 16:35:40 +0530

    Update disjoint-set-class.cpp

--- LeetCode/algo/disjoint-set-class.cpp
+++ LeetCode/algo/disjoint-set-class.cpp
@@ -1,71 +1,81 @@
 //https://leetcode.com/problems/count-the-number-of-complete-components/?envType=daily-question&envId=2025-03-22
 
+class DisjointUnion {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    DisjointUnion(int n) : parent(n, -1), rank(n, 1) {
+
+    }
+
+    int findParent(int node) {
+        if(parent[node] == -1) return node;
+        int p = findParent(parent[node]);
+        return p;
+    }
+
+    void unionByRank(int a, int b) {
+        int parentA = findParent(a);
+        int parentB = findParent(b);
+        if(parentA == parentB) {
+            return;
+        }
+
+        if(rank[parentA] > rank[parentB]) {
+            parent[parentB] = parentA;
+            rank[parentA] += rank[parentB];
+        }else {
+            parent[parentA] = parentB;
+            rank[parentB] += rank[parentA];
+        }
+        return;
+    }
+
+};
+
 class Solution {
+
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        // Initialize Union Find and edge counter
-        UnionFind dsu(n);
-        unordered_map<int, int> edgeCount;
-
+        DisjointUnion dsu(n);
+        unordered_map<int, int> componentParentToEdgesMap;
+        
         // Connect components using edges
-        for (const auto& edge : edges) {
-            dsu.unionSets(edge[0], edge[1]);
+        for(vector<int>& edge : edges) {
+            int u = edge[0], v = edge[1];
+            dsu.unionByRank(u, v);
         }
 
         // Count edges in each component
-        for (const auto& edge : edges) {
-            int root = dsu.find(edge[0]);
-            edgeCount[root]++;
+        for(vector<int>& edge : edges) {
+            int u = edge[0], v = edge[1];
+            int parentU = dsu.findParent(u);
+            componentParentToEdgesMap[parentU]++;
         }
-
+        
         // Check if each component is complete
-        int completeCount = 0;
-        for (int vertex = 0; vertex < n; vertex++) {
-            if (dsu.find(vertex) == vertex) {  // If vertex is root
-                int nodeCount = dsu.rank[vertex];
-                int expectedEdges = (nodeCount * (nodeCount - 1)) / 2;
-                if (edgeCount[vertex] == expectedEdges) {
-                    completeCount++;
+        int res = 0;
+        for(int i=0;i<n;i++){
+            if(dsu.parent[i] == -1) {
+                int edges = componentParentToEdgesMap[i];
+                int nodes = dsu.rank[i];
+                if(edges == (nodes*(nodes-1))/2) {
+                    res++;
                 }
             }
+            
         }
-        return completeCount;
+        return res;
     }
-
-private:
-    class UnionFind {
-    public:
-        vector<int> parent;
-        vector<int> rank;  // Tracks rank of each component
-
-        // Constructor with Initializer List
-        UnionFind(int n) : parent(n, -1), rank(n, 1) {}
-
-        // Find root of component with path compression
-        int find(int node) {
-            if (parent[node] == -1) {
-                return node;
-            }
-            return parent[node] = find(parent[node]);
-        }
-
-        // Union by size
-        void unionSets(int node1, int node2) {
-            int root1 = find(node1);
-            int root2 = find(node2);
-
-            if (root1 == root2) {
-                return;
-            }
-
-            // Merge smaller component into larger one
-            if (rank[root1] > rank[root2]) {
-                parent[root2] = root1;
-                rank[root1] += rank[root2];
-            } else {
-                parent[root1] = root2;
-                rank[root2] += rank[root1];
-            }
-        }
-    };
 };
+
+//n(n-1)/2 edges in a component
+//find diff components
+//find num of nodes in each component
+//for each edge - map<edge to component> 
+//map<componentid, num of nodes>
+//map<componentid, edgeCount>
+//each component should have n*(n-1)/2 edges
+
+//input could be like [1,2], [3,4], [1,3] -- 2 components may be merged later on
