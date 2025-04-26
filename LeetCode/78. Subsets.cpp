87a2d46a20dbe26f80aa35fa91afe23b7de94af3
Sajal Agrawal <16614262+sajalagrawal@users.noreply.github.com>
2025-04-26 19:28:03 +0530

    Update 78. Subsets.cpp

--- LeetCode/78. Subsets.cpp
+++ LeetCode/78. Subsets.cpp
@@ -1,5 +1,34 @@
 //https://leetcode.com/problems/subsets/
 
+//1
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        
+        generateSubsets(nums, 0, current, result);
+        return result;
+    }
+
+    void generateSubsets(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
+        if(index == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+
+        //pick nums[index]
+        current.push_back(nums[index]);
+        generateSubsets(nums, index+1, current, result);
+
+        //dont pick nums[index]
+        current.pop_back();
+        generateSubsets(nums, index+1, current, result);
+    }
+};
+
+
+//2
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
